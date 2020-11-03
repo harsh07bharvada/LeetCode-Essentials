6b64f4fec11f601eb7e2c6757086e365e06d3fbc
Harsh Bharvada <harsh07bharvada@gmail.com>
2020-11-04 00:59:44 +0530

    Linked List cycle

---
+++ 141. Linked List cycle/js1.js
@@ -0,0 +1,16 @@
+var hasCycle = function(head) {
+    
+    let slow = head;
+    let fast = head;
+    
+    while (fast && fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            return true;
+        }
+    }
+    
+    return false;
+    
+};
