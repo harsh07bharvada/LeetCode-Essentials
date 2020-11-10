dcfdaa51595a1a9b90d8c2f56c0c24b11e7f2976
Harsh Bharvada <harsh07bharvada@gmail.com>
2020-11-10 16:26:15 +0530

    kth Largest Element in Array

---
+++ 215. kth Largest Element Array/js1.js
@@ -0,0 +1,56 @@
+function PriorityQueue (){
+    collections = [];
+    
+    this.enqueue = function(priority){
+        if(!collections.length){
+            collections.push(priority);
+        }else{
+            let added = false;
+            for(let i = 0;i< collections.length;i++){
+                if(collections[i] < priority){
+                    collections.splice(i,0,priority);
+                    added = true;
+                    break;
+                }
+            }
+            if(!added){
+                collections.push(priority);
+            }
+        }
+    }
+    
+    this.dequeue = function(){
+        if(collections.length)
+            collections.pop();
+    }
+    
+    this.size = function() {
+        return collections.length;
+    }
+    
+    this.print = function(){
+        console.log(collections);
+    }
+    this.getQueue = function(){
+        return collections;
+    }
+}
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    let priorityQ = new PriorityQueue();
+    
+    for(let n of nums){
+       priorityQ.enqueue(n);
+        if(priorityQ.size() > k){
+            priorityQ.dequeue();
+        }
+    }
+    
+    const list = priorityQ.getQueue();
+    return(list[list.length -1]);
+    
+};
