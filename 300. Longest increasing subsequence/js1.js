92d93c7f5ef3513c1b2acb39afd121227c09d876
Harsh Bharvada <harsh07bharvada@gmail.com>
2020-12-18 19:46:46 +0530

    LOngest increasing subsequence

---
+++ 300. Longest increasing subsequence/js1.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    
+    if(nums.length <= 1)
+        return nums.length;
+
+    let buckets = [nums[0]];
+  
+     const binarySearch = val =>{
+        let low = 0, high = buckets.length - 1;
+        while(low < high){
+            
+            const mid = low + Math.floor((high - low)/2);
+            if(buckets[mid]< val)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return low;
+    }
+     
+    for(let i=1;i < nums.length;i++){
+        if(buckets[buckets.length -1] < nums[i]){
+            buckets.push(nums[i]);
+        }else{
+            const index = binarySearch(nums[i]);
+            
+            buckets[index] = nums[i];
+        }
+    }
+    return buckets.length;
+};
